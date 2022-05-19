d593e7199d2afef8d9f617f44d6dbb6369c788c0
Tuanbk <dodinhtuan681@gmail.com>
2022-05-19 15:48:21 +0700

    init project

---
+++ CSS Music/js/music-list.js
@@ -0,0 +1,51 @@
+let allMusic = [
+    {
+      name: "Có hẹn với thanh xuân",
+      artist: "Monstar",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Ikson Anywhere – Ikson",
+      artist: "Audio Library",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Beauz & Jvna - Crazy",
+      artist: "Beauz & Jvna",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Hardwind - Want Me",
+      artist: "Mike Archangelo",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "Jim - Sun Goes Down",
+      artist: "Jim Yosef x Roy",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: "Lost Sky - Vision NCS",
+      artist: "NCS Release",
+      img: "music-6",
+      src: "music-6"
+    },
+    {
+      name: "Người em cố đô",
+      artist: "Rum x Đaa",
+      img: "music-7",
+      src: "music-7"
+    },
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+  ];
